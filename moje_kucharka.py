9afa8181a0ca8c7fdc648574c1e6f795e3009b04
PetraHrad <petra.hradecky@gmail.com>
2021-03-07 16:30:37 +0100

    pokračuji

--- moje_kucharka.py
+++ moje_kucharka.py
@@ -9,21 +9,43 @@ import re
 
 print('Vítej v kuchařce', os.getlogin())
 
+# input uživatele jméno a příjmení jestli uživatel zadá správně vypsat hlášku
+# jestliže nezadá aspon písmeno ukončí se program
 
-#input uživatele jméno a příjmení jestli uživatel zadá správně vypsat hlášku jestliže nezadá aspon písmeno ukončí se program
-jmeno = input("Zadej jméno: ")
-re.search('[a-zA-Z]', jmeno)
 
-if (re.search('[a-zA-Z]', jmeno)):
-  print("Můžete pokračovat zadáním příjmení")
-else: print("špatně")
+def kotrola_pismen():
+  while True:
+    jmeno = input("Zadej jméno: ")
+    if jmeno.isalpha():
+        print("Můžete pokračovat zadáním příjmení")
+        break
+    else:
+        print("Jméno obsahuje číslice/znaky, prosím o zadání jména bez číslic/znaky: ")
+        continue
+  while True:
+    prijmeni = input("Zadej příjmení: ")
+    if prijmeni.isalpha():
+        print("Můžete pokračovat zadáním věku")
+        break
+    else:
+        print("Příjmení obsahuje číslice/znaky, prosím o zadání příjmení bez číslic/znaky: ")
+        continue
 
-prijmeni = input("Zadej příjmení: ")
-print(prijmeni)
-kontrola1 = re.search('[a-zA-Z]', prijmeni,)
-if (re.search('[a-zA-Z]', prijmeni)):
-  print("Jméno a příjmení bylo zadáno správně")
-else: print("špatně")
+
+while True:
+    try:
+        vek = int(input("Prosím zadejte svůj věk: "))
+    except ValueError:
+        print("Prosím o zadání znovu:")
+        continue
+    else:
+        break
+if vek >= 12:
+    print("Můžete pokračovat dále")
+else:
+    print("Omlouváme se, ale tyto informace jsou dostupné jen pro starší 12let")
+
+
 
 
 
---
+++ nepouzitelne.py
@@ -0,0 +1,11 @@
+#re.search('[a-zA-Z]', jmeno)
+#if (re.search('[a-zA-Z]', jmeno)):
+ # print("Můžete pokračovat zadáním příjmení")
+#else:
+  #print("špatně")
+
+#kontrola1 = re.search('[a-zA-Z]', prijmeni,)
+#if (re.search('[a-zA-Z]', prijmeni)):
+# print("Jméno a příjmení bylo zadáno správně")
+#else: print("špatně")
+
